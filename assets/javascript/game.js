58d4c0e4227130ee2e94bcd5001d5a834fd581cd
unknown <sarah.lopriore@gmail.com>
2018-08-24 21:41:27 -0400

    generate random numbers for each crystal using jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,18 +14,42 @@ var lose = 0;
 
 
 // computer chooses a random number between 19 and 120 
+// random number is displayed in #randomNumber section
 var chooseNumber = function() {
-    computerNumber = Math.floor(Math.random() * 101) + 19;
+    computerNumber = Math.floor(Math.random() * 102) + 19;
     randomNumberSec.text(computerNumber);
 }
 
-chooseNumber();
-// random number is displayed in #randomNumber section
-
 // computer chooses a random number between 1  and 12 for pink crystal
+var pickPink = function() {
+    pinkNumber = Math.floor(Math.random() * 12) + 1;
+    console.log("pink crystal " + pinkNumber);
+}
+
 // computer chooses a random number between 1  and 12 for orange crystal
+var pickOrange = function() {
+    orangeNumber = Math.floor(Math.random() * 11) + 1;
+    console.log("orange crystal " + orangeNumber);
+}
+
 // computer chooses a random number between 1  and 12 for blue crystal
+var pickBlue = function() {
+    blueNumber = Math.floor(Math.random() * 11) + 1;
+    console.log("blue crystal " + blueNumber);
+}
+
 // computer chooses a random number between 1  and 12 for purple crystal
+var pickPurple = function() {
+    purpleNumber = Math.floor(Math.random() * 11) + 1;
+    console.log("purple crystal " + purpleNumber);
+}
+
+
+chooseNumber();
+pickPink();
+pickOrange();
+pickBlue();
+pickPurple();
 
 // when player clicks pink crystal increase the total score by random number assigned to pink crystal
 // display new total score in #totalScore paragraph
